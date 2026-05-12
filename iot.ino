506bb729b908355a9fab0a6158835fa0e16a5e5c
Ajith Biju <bajith2006@gmail.com>
2026-05-12 09:38:04 +0530

    Update iot.ino

--- iot.ino
+++ iot.ino
@@ -1,152 +1,70 @@
+#define BLYNK_TEMPLATE_ID "YourTemplateID" //enter ur template id
+#define BLYNK_TEMPLATE_NAME "HomeAutomation"
+#define BLYNK_AUTH_TOKEN "YourAuthToken"  //Enter ur authentification token
+
 #include <WiFi.h>
 #include <BlynkSimpleEsp32.h>
 
-#define BLYNK_PRINT Serial
+char ssid[] = "YourWiFiName";  //Enter ur wifi name
+char pass[] = "YourWiFiPassword";  //Enter ur wifi password
 
-char auth[] = "YourAuthToken";
-char ssid[] = "YourWiFiName";
-char pass[] = "YourWiFiPassword";
+// Relay pins
+int lightRelay = 5;
+int fanRelay = 18;
 
-#define relay1 5
-#define relay2 18
-#define relay3 19
-#define relay4 21
-
-#define led1 2
-#define buzzer 15
-
-int sensorPin = 34;
-int sensorValue = 0;
-
-BlynkTimer timer;
-
-void checkWiFiStatus() {
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("WiFi Disconnected");
-    digitalWrite(buzzer, HIGH);
-    delay(200);
-    digitalWrite(buzzer, LOW);
-  } else {
-    Serial.println("WiFi Connected");
-  }
-}
-
-void readSensor() {
-  sensorValue = analogRead(sensorPin);
-
-  Serial.print("Sensor Value: ");
-  Serial.println(sensorValue);
-
-  Blynk.virtualWrite(V5, sensorValue);
-
-  if (sensorValue > 2000) {
-    digitalWrite(led1, HIGH);
-  } else {
-    digitalWrite(led1, LOW);
-  }
-}
-
-BLYNK_WRITE(V0) {
-  int state = param.asInt();
-
-  if (state == 1) {
-    digitalWrite(relay1, HIGH);
-    Serial.println("Relay 1 ON");
-  } else {
-    digitalWrite(relay1, LOW);
-    Serial.println("Relay 1 OFF");
-  }
-}
-
-BLYNK_WRITE(V1) {
-  int state = param.asInt();
-
-  if (state == 1) {
-    digitalWrite(relay2, HIGH);
-    Serial.println("Relay 2 ON");
-  } else {
-    digitalWrite(relay2, LOW);
-    Serial.println("Relay 2 OFF");
-  }
-}
-
-BLYNK_WRITE(V2) {
-  int state = param.asInt();
-
-  if (state == 1) {
-    digitalWrite(relay3, HIGH);
-    Serial.println("Relay 3 ON");
-  } else {
-    digitalWrite(relay3, LOW);
-    Serial.println("Relay 3 OFF");
-  }
-}
-
-BLYNK_WRITE(V3) {
-  int state = param.asInt();
-
-  if (state == 1) {
-    digitalWrite(relay4, HIGH);
-    Serial.println("Relay 4 ON");
-  } else {
-    digitalWrite(relay4, LOW);
-    Serial.println("Relay 4 OFF");
-  }
-}
-
-void startupAnimation() {
-  digitalWrite(led1, HIGH);
-  delay(200);
-  digitalWrite(led1, LOW);
-  delay(200);
-  digitalWrite(led1, HIGH);
-  delay(200);
-  digitalWrite(led1, LOW);
-}
-
-void setup() {
-
+void setup()
+{
   Serial.begin(115200);
 
-  pinMode(relay1, OUTPUT);
-  pinMode(relay2, OUTPUT);
-  pinMode(relay3, OUTPUT);
-  pinMode(relay4, OUTPUT);
+  // Set relay pins as output
+  pinMode(lightRelay, OUTPUT);
+  pinMode(fanRelay, OUTPUT);
 
-  pinMode(led1, OUTPUT);
-  pinMode(buzzer, OUTPUT);
+  // Initially OFF
+  digitalWrite(lightRelay, LOW);
+  digitalWrite(fanRelay, LOW);
 
-  digitalWrite(relay1, LOW);
-  digitalWrite(relay2, LOW);
-  digitalWrite(relay3, LOW);
-  digitalWrite(relay4, LOW);
+  // Connect to Blynk
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 
-  startupAnimation();
-
-  WiFi.begin(ssid, pass);
-
-  Serial.print("Connecting to WiFi");
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi Connected Successfully");
-
-  Blynk.begin(auth, ssid, pass);
-
-  timer.setInterval(2000L, readSensor);
-  timer.setInterval(5000L, checkWiFiStatus);
-
-  Serial.println("ESP32 IoT Home Automation Started");
+  Serial.println("Connected to Blynk");
 }
 
-void loop() {
+// Light Control
+BLYNK_WRITE(V0)
+{
+  int value = param.asInt();
 
+  if(value == 1)
+  {
+    digitalWrite(lightRelay, HIGH);
+    Serial.println("Light ON");
+  }
+  else
+  {
+    digitalWrite(lightRelay, LOW);
+    Serial.println("Light OFF");
+  }
+}
+
+// Fan Control
+BLYNK_WRITE(V1)
+{
+  int value = param.asInt();
+
+  if(value == 1)
+  {
+    digitalWrite(fanRelay, HIGH);
+    Serial.println("Fan ON");
+  }
+  else
+  {
+    digitalWrite(fanRelay, LOW);
+    Serial.println("Fan OFF");
+  }
+}
+
+void loop()
+{
   Blynk.run();
-
-  timer.run();
-
 }
